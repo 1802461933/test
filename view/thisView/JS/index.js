5fc7e09dca14e2f7f00c08e1fa0e276f0d0e59eb
1802461933 <1802461933@qq.com>
2017-08-11 18:41:17 +0800

    add Customer list

--- view/thisView/JS/index.js
+++ view/thisView/JS/index.js
@@ -4,9 +4,13 @@ var sid = document.getElementById("sidebar");
 var sidButton = document.getElementById("sidButton");
 var heigth = footer.offsetTop - header.offsetHeight ;
 var butimg = sidButton.firstElementChild;
+var nav = document.getElementById('nav');
+var content = document.getElementById("content");
 butimg.style.marginTop = heigth / 2.2 + "px";
 sid.style.height = heigth+ "px";
 sidButton.style.height = heigth+ "px";
+content.style.height = heigth + "px";
+console.log(content.offsetLeft);
 sid.style.marginTop = header.offsetHeight;
 sidButton.style.marginTop = header.offsetHeight+"px";			
 butimg.onclick = function(){
@@ -14,10 +18,14 @@ var sidWidth = sid.offsetWidth;
 	if(sidWidth==0){
 		sid.style.width = 187+"px";
 		this.src="../../img/index/bar_close.gif";
+		nav.style.display="block";
+		content.style.width="86.5%";
 	}else{
-		sid.style.width = 0 + "px"; 
+			nav.style.display="none";
+			sid.style.width = 0 + "px"; 
 			this.src = "../../img/index/bar_right.gif";
-			}
+			content.style.width="99.5%";
+		}
 	}
 function callStatus(btn){
 	if(btn.innerText == "空闲"){
@@ -26,6 +34,16 @@ function callStatus(btn){
 	}else{
 		btn.style.backgroundColor="#1E9FFF";
 		btn.innerText="空闲";
+	}
+	
+}
+
+function hiddenFromSenior(){
+	var fromSeniorHeight = document.getElementById("fromSenior").offsetHeight;
+	if( fromSeniorHeight == 0){
+		document.getElementById("fromSenior").style.height = 200 + "px";
+	}else{
+		document.getElementById("fromSenior").style.height = 0 + "px";
 	}
 	
 }
