4c05bb7cda89943b1cc7750b9fc4bc0028176c80
1802461933 <1802461933@qq.com>
2017-08-12 18:02:19 +0800

    add time Model

--- view/thisView/JS/index.js
+++ view/thisView/JS/index.js
@@ -15,6 +15,8 @@ sid.style.marginTop = header.offsetHeight;
 sidButton.style.marginTop = header.offsetHeight+"px";			
 butimg.onclick = function(){
 var sidWidth = sid.offsetWidth;
+
+/*动态布局*/
 	if(sidWidth==0){
 		sid.style.width = 187+"px";
 		this.src="../../img/index/bar_close.gif";
@@ -27,6 +29,8 @@ var sidWidth = sid.offsetWidth;
 			content.style.width="99.5%";
 		}
 	}
+
+/*示忙状态替换*/
 function callStatus(btn){
 	if(btn.innerText == "空闲"){
 		btn.style.backgroundColor="#FF5722";
@@ -38,12 +42,21 @@ function callStatus(btn){
 	
 }
 
+/*显示隐藏高级搜索*/
 function hiddenFromSenior(){
 	var fromSeniorHeight = document.getElementById("fromSenior").offsetHeight;
 	if( fromSeniorHeight == 0){
 		document.getElementById("fromSenior").style.height = 200 + "px";
+		document.getElementById("fromSenior").style.display = "block";
 	}else{
 		document.getElementById("fromSenior").style.height = 0 + "px";
+		document.getElementById("fromSenior").style.display = "none";
 	}
+	
+}
+
+function tiemday(btn){
+	layui.laydate({elem: btn,format:'YYYY-MM-DD hh:mm:ss',istoday: false});
+	btn.value=laydate.now(-30, 'YYYY-MM-DD hh:mm:ss');
 	
 }
